ff05053e0c2a629a6490dd77cc7b3705cb2f4ccb
aldrinmc <aldrinmc143@gmail.com>
2015-01-22 18:10:37 +0800

    Sprint 1: menu page and stage page

---
+++ src/main/java/com/example/aldrinmcadelia/myapplication2/StagePage.java
@@ -0,0 +1,42 @@
+package com.example.aldrinmcadelia.myapplication2;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class StagePage extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stage_page);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void backPage(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    public void stageOne(View view){
+        Intent intent = new Intent(this, stageone.class);
+        startActivity(intent);
+    }
+}
