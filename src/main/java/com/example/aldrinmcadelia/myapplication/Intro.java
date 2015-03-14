255748643112bb66c4e8964aa746bc980e37415a
aldrinmc <aldrinmc143@gmail.com>
2015-03-14 03:07:22 -0700

    with intro activity and music for intro and stages

--- src/main/java/com/example/aldrinmcadelia/myapplication/Intro.java
+++ src/main/java/com/example/aldrinmcadelia/myapplication/Intro.java
@@ -1,39 +1,91 @@
 package com.example.aldrinmcadelia.myapplication;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.File;
 
 
-public class Intro extends ActionBarActivity {
+public class Intro extends Activity {
+
+    MediaPlayer OurSong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        OurSong = MediaPlayer.create(Intro.this,R.raw.ants);
+        OurSong.setLooping(true);
+        OurSong.start();
+
+        String fontPath = "Lobster_1.3.otf";
+
+        TextView tittle = (TextView) findViewById(R.id.Title_view);
+        TextView play = (TextView) findViewById(R.id.buttonPlay);
+        TextView about = (TextView) findViewById(R.id.buttonAbout);
+        TextView BestTime = (TextView) findViewById(R.id.BestTime);
+        TextView exit = (TextView) findViewById(R.id.buttonExit);
+
+        Typeface tittleView =  Typeface.createFromAsset(getAssets(), fontPath);
+        Typeface playView = Typeface.createFromAsset(getAssets(), fontPath);
+        Typeface aboutView = Typeface.createFromAsset(getAssets(), fontPath);
+        Typeface exitView = Typeface.createFromAsset(getAssets(), fontPath);
+        Typeface Best = Typeface.createFromAsset(getAssets(), fontPath);
+
+        tittle.setTypeface(tittleView);
+        play.setTypeface(playView);
+        about.setTypeface(aboutView);
+        exit.setTypeface(exitView);
+        BestTime.setTypeface(Best);
+
+        String filename = "SaveFile";
+        String content = "Score";
+        File file = null;
+
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_intro, menu);
-        return true;
+
+
+    public void StageMenu(View view) {
+
+        Intent intent = new Intent(this, StagePage.class);
+        startActivity(intent);
+        finish();
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void About(View view) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        Intent intent = new Intent (this, AboutPage.class);
+        startActivity(intent);
+        finish();
 
-        return super.onOptionsItemSelected(item);
     }
+
+    public void TheBestTime(View view){
+        Intent intent = new Intent(this,BestTime.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void ExitApp(View view){
+        finish();
+        System.exit(0);
+    }
+
+    protected void onPause(){
+        super.onPause();
+        OurSong.release();
+        finish();
+    }
+
 }
--- src/main/java/com/example/aldrinmcadelia/myapplication/MainActivity.java
+++ src/main/java/com/example/aldrinmcadelia/myapplication/MainActivity.java
@@ -27,64 +27,24 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        OurSong = MediaPlayer.create(MainActivity.this,R.raw.ants);
-        OurSong.setLooping(true);
+        OurSong = MediaPlayer.create(MainActivity.this,R.raw.intro);
         OurSong.start();
 
-        String fontPath = "Lobster_1.3.otf";
+        Thread timer = new Thread(){
+            public void run(){
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    Intent openStartingPoint = new Intent(com.example.aldrinmcadelia.myapplication.MainActivity.this,Intro.class);
+                    startActivity(openStartingPoint);
+                    overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+                }
+            }
+        };
+        timer.start();
 
-        TextView tittle = (TextView) findViewById(R.id.Title_view);
-        TextView play = (TextView) findViewById(R.id.buttonPlay);
-        TextView about = (TextView) findViewById(R.id.buttonAbout);
-        TextView BestTime = (TextView) findViewById(R.id.BestTime);
-        TextView exit = (TextView) findViewById(R.id.buttonExit);
-
-        Typeface tittleView =  Typeface.createFromAsset(getAssets(), fontPath);
-        Typeface playView = Typeface.createFromAsset(getAssets(), fontPath);
-        Typeface aboutView = Typeface.createFromAsset(getAssets(), fontPath);
-        Typeface exitView = Typeface.createFromAsset(getAssets(), fontPath);
-        Typeface Best = Typeface.createFromAsset(getAssets(), fontPath);
-
-        tittle.setTypeface(tittleView);
-        play.setTypeface(playView);
-        about.setTypeface(aboutView);
-        exit.setTypeface(exitView);
-        BestTime.setTypeface(Best);
-
-        String filename = "SaveFile";
-        String content = "Score";
-        File file = null;
-
-    }
-
-
-
-
-    public void StageMenu(View view) {
-
-        Intent intent = new Intent(this, StagePage.class);
-        startActivity(intent);
-        finish();
-
-    }
-
-    public void About(View view) {
-
-        Intent intent = new Intent (this, AboutPage.class);
-        startActivity(intent);
-        finish();
-
-    }
-
-    public void TheBestTime(View view){
-        Intent intent = new Intent(this,BestTime.class);
-        startActivity(intent);
-        finish();
-    }
-
-    public void ExitApp(View view){
-        finish();
-        System.exit(0);
     }
 
     protected void onPause(){
